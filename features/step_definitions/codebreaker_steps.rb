ca9fb024949da2eac7b1cb140b149e90e239d3bd
Justin Tanacredi <Jtanacredi@gmail.com>
2014-02-09 13:36:40 -0500

    Add guess to game with cucumber/rspec

--- features/step_definitions/codebreaker_steps.rb
+++ features/step_definitions/codebreaker_steps.rb
@@ -16,8 +16,8 @@ Given /^I am not yet playing$/ do
 end
 
 Given /^the secret code is "([^"]*)"$/ do |secret|
-  game = Codebreaker::Game.new(output)
-  game.start(secret)
+  @game = Codebreaker::Game.new(output)
+  @game.start(secret)
 end
 
 When /^I start a new game$/ do
@@ -25,8 +25,14 @@ When /^I start a new game$/ do
   game.start('1234')
 end
 
+When /^I guess "([^"]*)"$/ do |guess|
+  @game.guess(guess)
+end
+
 Then /^I should see "([^"]*)"$/ do |message|
   output.messages.should include(message)
 end
 
-
+Then /^the mark should be "([^"]*)"$/ do |mark|
+  output.messages.should include(mark)
+end
