9a03d15144a43617ef47af670ea50e680e93f1c0
Justin Tanacredi <Jtanacredi@gmail.com>
2014-02-23 00:16:21 -0500

    fix duplicate number issue

--- lib/codebreaker/marker.rb
+++ lib/codebreaker/marker.rb
@@ -15,17 +15,29 @@ class Marker
   end
 
   def number_match_count
-    (0..3).inject(0) do |count, index|
-      count + (number_match?(index) ? 1 : 0)
-      #number_match_count = 0
-      #(0..3).each do |index|
-      #  if number_match?(guess, index)
-      #    number_match_count += 1
-      #  end
-    end
-    #number_match_count
+   total_match_count - exact_match_count
+    #(0..3).inject(0) do |count, index|
+    #  count + (number_match?(index) ? 1 : 0)
+    #  #number_match_count = 0
+    #  #(0..3).each do |index|
+    #  #  if number_match?(guess, index)
+    #  #    number_match_count += 1
+    #  #  end
+    #end
+    ##number_match_count
   end
 
+  def total_match_count
+    count = 0
+    secret = @secret.split('')
+    @guess.split('').map do |n|
+      if @secret.include?(n)
+        @secret.delete_at(@secret.index(n))
+        count += 1
+      end
+    end
+    count
+  end
 
   def exact_match?(index)
     @guess[index] == @secret[index]
